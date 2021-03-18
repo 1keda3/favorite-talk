a0cad1b3462dbece76d606e237c1f32b9f22fafb
1keda3 <xrzrutosa@yahoo.co.jp>
2021-03-18 16:15:27 +0900

    ルームのHTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'rooms/index'
+  root to: "rooms#index"
+  resources :rooms, only: [:index, :create, :new] do
+    resources :messages, only: [:index, :create, :new]
+  end
 end
